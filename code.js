40bb295b8085f6515d74586288f006a05282caea
MarioLopB <mariolopez082003@gmail.com>
2023-03-12 13:54:39 +0100

    Making javascript code

---
+++ code.js
@@ -0,0 +1,15 @@
+const num = document.getElementById('num');
+const playBtn = document.getElementById('play-btn');
+
+let fb = (n) => {
+    let text = "";
+
+    for(let i = 1; i <= n; i++){
+        text += i % 3 == 0 ? "Fizz" 
+        : i % 5 == 0 ? "Buzz" 
+        : i % 3 == 0 && i % 5 == 0 ? "Fizz Buzz"
+        : i;
+    }
+
+    return text.split(" ");
+}
